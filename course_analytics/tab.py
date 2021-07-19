f1452e17f9976657e434a1c913985a7541aa1360
Artem <artem.marshalkin@gmail.com>
2021-07-19 17:57:15 +0300

    remove check permissions

--- course_analytics/tab.py
+++ course_analytics/tab.py
@@ -1,6 +1,7 @@
 from lms.djangoapps.courseware.tabs import CourseTab
 from django.utils.translation import ugettext_noop
 
+
 class CourseAnalyticsTab(CourseTab):
     """A new course tab."""
 
@@ -15,7 +16,7 @@ class CourseAnalyticsTab(CourseTab):
     @classmethod
     def is_enabled(cls, course, user=None):
         """Returns true if this tab is enabled."""
-        return bool(user and user.is_authenticated and user.has_perm(permissions.VIEW_DASHBOARD, course.id))
+        return bool(user and user.is_authenticated)
         #return True
 
     @classmethod
